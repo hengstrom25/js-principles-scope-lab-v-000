486fc7510c7a4919c7dea92ba98e598ca611ba46
Heidi Engstrom <hengstrom25@gmail.com>
2018-10-24 13:50:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 
 var customerName = 'bob';
 
-function upperCaseCustomerName(customerName) {
-  customerName.toUpperCase(customerName);
+function upperCaseCustomerName(str) {
+  str.toUpperCase(customerName);
 }
